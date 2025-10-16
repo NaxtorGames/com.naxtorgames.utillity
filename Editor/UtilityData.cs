1b68dabad5706d9ef2e201804910770386a7e987
NaxtorGames <72697963+NaxtorGames@users.noreply.github.com>
2025-10-16 22:07:11 +0200

    Window menu item constant added.

--- Editor/UtilityData.cs
+++ Editor/UtilityData.cs
@@ -3,5 +3,6 @@
     public static class UtilityData
     {
         public const string TOOL_MENU_PATH = "Tools/NaxtorGames/";
+        public const string WINDOW_MENU_PATH = "Window/NaxtorGames/";
     }
 }
